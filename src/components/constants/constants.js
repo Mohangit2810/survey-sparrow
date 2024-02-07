6098442edfff4f649ca0f1707ebc53686bbd526b
Mohangit2810 <mohanraji2810@gmail.com>
2024-02-07 19:00:18 +0530

    minor changes in constant

--- src/components/constants/constants.js
+++ src/components/constants/constants.js
@@ -1,5 +1,4 @@
-export const chatGptToken =
-  "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";
+export const chatGptToken = "YOUR_OPEN_AI_API_KEY";
 export const chatGptUrl = "https://api.openai.com/v1/chat/completions";
 export const url = "https://api.surveysparrow.com";
 export const seperator = ":-:";
